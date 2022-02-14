0c7fddd9fb80b9ab47c9549d964a5aa685a95933
FicusFocus <shkurihin.pashok@mail.ru>
2022-02-15 05:08:55 +0600

    .

---
+++ TowerDifence/Assets/Scripts/Enemy/EnemyHalthBar.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHalthBar : MonoBehaviour
+{
+    [SerializeField] private Sprite _helthBarSprite;
+}
---
+++ TowerDifence/Assets/Scripts/UI/MoneyView.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class MoneyView : MonoBehaviour
+{
+    [SerializeField] private Player _player;
+}
